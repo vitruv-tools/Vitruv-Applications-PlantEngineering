2f3cfa3d6427783fc01da18c16668f16387773d2
Andreas222 <a_lang@outlook.de>
2018-09-06 11:36:55 +0200

    adding some checks for routines, bugfixing

---
+++ utils/tools.vitruv.applications.automationml.utils/src/tools/vitruv/applications/automationml/utils/externalInterfaceIncluded/SystemUnitClassForInterfaceChecker.java
@@ -0,0 +1,17 @@
+package tools.vitruv.applications.automationml.utils.externalInterfaceIncluded;
+
+import org.automationml.caex.caex.ExternalInterface;
+import org.automationml.caex.caex.SystemUnitClass;
+
+public class SystemUnitClassForInterfaceChecker {
+	public static boolean checkForExternalPLCInterface(SystemUnitClass systemClass) {
+		//TODO
+		return true;
+	}
+	
+	//RefUri Attribut als erstes Attribut des ExternalInterfaces
+	public static ExternalInterface getExternalPLCInterface(SystemUnitClass systemClass) {
+		//TODO
+		return null;
+	}
+}
