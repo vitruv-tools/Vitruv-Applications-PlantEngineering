430b1b7a7b0a94053c2f85eeebffbcc1fe07535e
Andreas222 <a_lang@outlook.de>
2018-10-23 10:16:52 +0200

    improve tags for correspondences

--- utils/tools.vitruv.applications.automationml.utils/src/tools/vitruv/applications/automationml/utils/amlutils/TagHelper.java
+++ utils/tools.vitruv.applications.automationml.utils/src/tools/vitruv/applications/automationml/utils/amlutils/TagHelper.java
@@ -1,5 +1,12 @@
 package tools.vitruv.applications.automationml.utils.amlutils;
 
 public class TagHelper {
-	public static final String TAG = "aTag";
+	//public static final String TAG = "aTag";	// Dummy-Tag
+	public static final String INTERFACE_SCENE = "TagForExternalInterfaceAndScene";				// Verknüpfung CAEX zu Collada
+	public static final String INTERFACE_POU = "TagForExternalInterfaceAndPou";					// Verknüpfung CAEX zu PLCopen
+	public static final String CLASS_INTERFACE = "TagForExternalInterfaceAndInterfaceClass";
+	public static final String PROTOTYPE_CLONE = "TagForSystemUnitClassAndInternalElement";		// Prototyp-Clone Beziehung
+	public static final String AML_CAEX = "TagForContainerAndCAEXFile";
+	public static final String AML_COLLADA = "TagForContainerAndColladaFile";
+	public static final String AML_PLCOPEN = "TagForContainerAndPLCopenFile";
 }
